3eca08ede2ed641956d267f02da294466a3e5925
sitijubed12 <44315113+sitijubed12@users.noreply.github.com>
2019-01-31 05:28:17 -0800

    Menentukan Nilai Ganjil dan Genap

---
+++ latihan1.1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x;
+  cout << "Masukan Bilangan :"<< endl;
+  cin>>x;
+    if (x%2==0)
+    cout<<" Bilangan yang anda masukkan adalah Genap";
+    else
+    cout<<"Bilangan yang anda masukan adalah Ganjil";
+    return 0;
+}
